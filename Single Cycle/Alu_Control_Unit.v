5047cfa3da99bb7106e1e402373c0a9f9c318b40
stefxici <97343745+stefxici@users.noreply.github.com>
2022-11-28 19:17:33 -0800

    swapped
    wrong code written in Alu_Control_Unit.v

--- Single Cycle/Alu_Control_Unit.v
+++ Single Cycle/Alu_Control_Unit.v
@@ -1,15 +1,28 @@
-module Imm_Sign_Extend(
+module ALU_Control_Unit(
     // System Clock
 
     // User Interface
-    input       [15:0]  Immediate,
-
-    output      [31:0]  SignImm
+    input       [5:0]   Funct,
+    input       [1:0]   ALUOp,
+    output  reg [2:0]   ALUControl
 );
-    assign  SignImm = {{16{Immediate[15]}}, Immediate[15:0]};
-endmodule
-<<<<<<< Updated upstream
-//
-=======
 
->>>>>>> Stashed changes
+    always @(*) begin
+        case (ALUOp)
+            2'b00: ALUControl = 3'b010;
+            2'b10: begin
+                case (Funct)
+                    6'b100000 : ALUControl = 3'b010;
+                    6'b100010 : ALUControl = 3'b110;
+                    6'b100100 : ALUControl = 3'b000;
+                    6'b100101 : ALUControl = 3'b001;
+                    6'b101010 : ALUControl = 3'b111;
+                endcase
+            end
+            2'b01: ALUControl = 3'b110;
+            default: ;
+        endcase
+    end
+
+
+endmodule
