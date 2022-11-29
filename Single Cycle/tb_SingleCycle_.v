a33742f8cbc4f5c837d1940caa622a421c448d21
stefxici <97343745+stefxici@users.noreply.github.com>
2022-11-28 19:27:40 -0800

    added in
    Mip_single_cycle
    Pc
    Register_file
    tb_SingleCycle

---
+++ Single Cycle/tb_SingleCycle_.v
@@ -0,0 +1,44 @@
+`timescale 1ps/1ps
+module tb_MIPS_Single_Cycle();
+
+reg clk,rst_n;
+reg [31:0] cnt;
+
+initial
+begin            
+    $dumpfile("MIPS_wave.vcd");       
+    $dumpvars(0, tb_MIPS_Single_Cycle);    
+end
+
+initial begin
+    clk <= 1'b0;
+    rst_n <= 1'b0;
+    cnt <= 32'b0;
+
+    #10
+    clk <= 1'b1;
+
+    #10
+    clk <= 1'b0;
+    rst_n <= 1'b1;
+
+    forever begin
+        #10 clk <= ~clk;
+    end
+
+end
+
+always @(negedge clk) begin
+    cnt <= cnt + 1'b1;
+    if (cnt >= 17) begin
+        $stop;
+    end
+end
+
+
+MIPS_Single_Cycle u_MIPS_Single_Cycle(
+    .clk(clk),
+    .rst_n(rst_n)
+);
+
+endmodule
